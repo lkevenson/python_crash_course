c78fb30559dbcdbc62bd4e47282a9728a2fde1a5
Kevenson Leger <50429391+lkevenson@users.noreply.github.com>
2024-04-10 17:24:16 -0400

    Exo 3.2

---
+++ chapter_3/exo_3.2.py
@@ -0,0 +1,6 @@
+names = ["Ederson", "Lorry", "Samari", "Duke"]
+
+print(f"Hello, {names[0]} from Quebec city")
+print(f"Hello, {names[1]} from Montreal")
+print(f"Hello, {names[2]} from St-Eustache")
+print(f"Hello, {names[-1]} from Port-au-Prince")
